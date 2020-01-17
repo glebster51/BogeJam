c074bdaf5743ad25ac3db4d9b45f372fc5a3c4ba
glebster51 <glebster51@rambler.ru>
2020-01-18 03:35:27 +0800

    импортнул спрайты

---
+++ Assets/G51/PixelSnapper.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+      [ExecuteInEditMode]
+public class PixelSnapper : MonoBehaviour
+      {
+            public int pixelPerUnit;
+      private void LateUpdate()
+      {
+            Vector2 pos = transform.localPosition;
+            if (pixelPerUnit > 0f)
+            {
+                  float step = 1f / pixelPerUnit;
+                              
+                  pos.x = Mathf.RoundToInt(pos.x / step) * step;
+                  pos.y = Mathf.RoundToInt(pos.y / step) * step;
+            }
+
+            transform.localPosition = pos;
+
+      }
+}
