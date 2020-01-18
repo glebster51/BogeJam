ff7ea571e8b89831db917d89ed36bfd0d7fe2030
postmesabbey2017 <59844417+postmesabbey2017@users.noreply.github.com>
2020-01-18 11:02:55 +0300

    edit: Movement. Wall Jump fixed

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -5,24 +5,42 @@ using UnityEngine;
 public class Move : MonoBehaviour
 {
     public float speed, speedj;
-    
+
     public Rigidbody2D rb;
-   
+
     public bool isGrounded;
-   
+  
+    public LayerMask ground;
     // Start is called before the first frame update
     void Start()
     {
-       
+
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
 
-    
+
     void Update()
     {
-        
+
+        Vector3 origin = transform.position + Vector3.up;
+
+        RaycastHit2D hit = Physics2D.CircleCast(origin, 1f, Vector2.down, 1f, ground);
+
+        if (hit)
+        {
+            
+            isGrounded = Vector2.Angle(hit.normal, Vector2.up) < 30;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+
+
+
+
         if (((Input.GetKeyDown(KeyCode.Space)) && (isGrounded == true)))
         {
 
@@ -32,22 +50,6 @@ public class Move : MonoBehaviour
         float h = Input.GetAxisRaw("Horizontal");
         Vector2 movement = new Vector2(h * speed, rb.velocity.y);
         rb.velocity = Vector2.Lerp(rb.velocity, movement, 1);
-
-
-    }
-    private void OnCollisionEnter2D(Collision2D other) {
-        if (other.gameObject.tag == "Ground")
-        {
-            isGrounded = true;
-        }
-    }
-
-    private void OnCollisionExit2D(Collision2D other)
-    {
-        if (other.gameObject.tag == "Ground")
-        {
-            isGrounded = false;
-        }
     }
 
 }
