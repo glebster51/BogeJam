4d0b9f189a89128cab2c01dd69c3a02b2b6388f6
glebster51 <glebster51@rambler.ru>
2020-01-18 23:44:31 +0800

    Скрипт смены материиалов для получения урона, спрайты гуся, импортнул SpriteShape

---
+++ Assets/G51/MaterialSwapper.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class MaterialSwapper : MonoBehaviour
+{
+    public bool hit;
+    bool lhit;
+
+    public List<SpriteMultiRenderer> spriteRenderers;
+    [Space]public Material default_baseMat;
+    public Material default_outlineMat;
+    public Material hit_baseMat;
+    public Material hit_outlineMat;
+
+    private void LateUpdate()
+    {
+        
+        if (lhit != hit)
+        {
+            if (hit)
+                SetHitMaterial();
+            else
+                SetDefaultMaterial();
+        }
+    }
+
+    public void SetDefaultMaterial()
+    {
+        foreach (SpriteMultiRenderer spriteRenderer in spriteRenderers)
+            if (spriteRenderer)
+            {
+                spriteRenderer.SetMaterial(default_baseMat, default_outlineMat);
+                spriteRenderer.SetOutlineColor(outlineColor);
+            }
+
+        lhit = hit = false;
+
+    }
+
+    public Color outlineColor;
+    public void SetHitMaterial()
+    {
+        foreach (SpriteMultiRenderer spriteRenderer in spriteRenderers)
+            if (spriteRenderer)
+            {
+                spriteRenderer.SetMaterial(hit_baseMat, hit_outlineMat);
+                outlineColor = spriteRenderer.outlineColor;
+                spriteRenderer.SetOutlineColor(Color.white);
+            }
+
+        lhit = hit = true;
+    }
+}
+
+
+
