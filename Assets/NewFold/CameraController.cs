961cb0e8409103f7b377702ceb0142ef9a5694e0
glebster51 <glebster51@rambler.ru>
2020-01-19 06:25:23 +0800

    ФИКС

--- Assets/NewFold/CameraController.cs
+++ Assets/NewFold/CameraController.cs
@@ -36,10 +36,9 @@ public class CameraController : MonoBehaviour
 
             Vector3 oldPos = transform.position;
             
-            Vector3 mousePos = Camera.main.
-            
-            Vector3 targetPos = target.transform.position;
-            Vector3 newPos = Vector3.SmoothDamp(oldPos, targetPos, ref refVel, 0.5f);
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 targetPos = Vector3.Lerp(target.transform.position , mousePos, 0.5F) + offset;
+            Vector3 newPos = Vector3.SmoothDamp(oldPos, targetPos, ref refVel, 0.1f);
             transform.position = new Vector3(newPos.x, newPos.y, transform.position.z);
             
             
