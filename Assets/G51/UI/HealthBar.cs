eacb73f7d859fbc6acb3b6d6664bee0f3fdc4196
glebster51 <glebster51@rambler.ru>
2020-01-19 07:49:44 +0800

    no message

---
+++ Assets/G51/UI/HealthBar.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class HealthBar : MonoBehaviour
+{
+    public float maxFillScale;
+    public Transform fill;
+    public SpriteRenderer rend;
+    public Gradient gradient;
+    
+
+    [Range(0f, 1f)] public float value;
+    private float lValue;
+    void LateUpdate()
+    {
+        if (lValue != value)
+            SetValue(value);
+    }
+
+
+    private void SetValue(float newValue)
+    {
+        value = lValue = newValue;
+        Refresh();
+    }
+
+    void Refresh()
+    {
+        if (fill)
+        {
+            Vector3 ls = fill.localScale;
+            fill.localScale = new Vector3(value * maxFillScale, ls.y, 1f); 
+        }
+
+        if (rend)
+        {
+            rend.color = gradient.Evaluate(value);
+        }
+    }
+}
