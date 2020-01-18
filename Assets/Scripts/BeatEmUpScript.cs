3c87f622227898e739e85bc9af73ebecd26ed831
postmesabbey2017 <59844417+postmesabbey2017@users.noreply.github.com>
2020-01-18 14:42:38 +0300

    Add: RaycastSystem

--- Assets/Scripts/BeatEmUpScript.cs
+++ Assets/Scripts/BeatEmUpScript.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 public class BeatEmUpScript : MonoBehaviour
 {
     public float PlayerHP;
+    public bool facingRight;
     public Animator playerAnimator;
+    private SpriteRenderer mySpriteRenderer;
+    public GameObject raybox;
+    private Vector2 positionFight;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,24 +21,58 @@ public class BeatEmUpScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if ((Input.GetKeyDown(KeyCode.B)))
+        if (Input.GetMouseButton(0))
         {
-            playerAnimator.SetTrigger("Hit");
+           // playerAnimator.SetTrigger("Hit");
+           
+            SendDmg();
+        }
+
+        var delta = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+
+        if (delta.x >= 0 && !facingRight)
+        {
+            transform.localScale = new Vector2(1, 1); // or activate look right some other way
+            facingRight = true;
+            positionFight = Vector2.right;
+        }
+        else if (delta.x < 0 && facingRight)
+        {
+            transform.localScale = new Vector2(-1, 1); // activate looking left
+            facingRight = false;
+            positionFight = Vector2.left;
         }
     }
+    
 
-    private void OnCollisionStay2D(Collision2D other)
+    private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.tag == "Enemy")
         {
-            Invoke("HurtScript", 1f);
-
+            StartCoroutine(Attacked());
             Debug.Log("Player gets hurt, Health:"+PlayerHP);
         }
     }
-
+    IEnumerator Attacked()
+    {
+        HurtScript();
+        yield return new WaitForSeconds(2f);
+    }
     void HurtScript()
     {
-        PlayerHP -= 1;
+        PlayerHP -= 10;
+    }
+
+    public void SendDmg()
+    {
+
+        RaycastHit2D hit = Physics2D.Raycast(raybox.transform.position, positionFight, 1f);
+        
+        if (hit) {
+        if (hit.transform.tag == "Enemy")
+            {
+                Debug.Log("Enemy Hit");
+            }
+        }
     }
 }
