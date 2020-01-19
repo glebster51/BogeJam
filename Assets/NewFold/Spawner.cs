ba072b91fba5d5168f2955dce9a524b4aa9fdfb5
postmesabbey2017 <59844417+postmesabbey2017@users.noreply.github.com>
2020-01-19 15:22:11 +0300

    Add: Spawner. Shadows/Arena fixed

---
+++ Assets/NewFold/Spawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] dudes;
+    GameObject player1;
+    private float SpawnTimer = 10.0f;
+    private float basecoord;
+    void start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        player1 = GameObject.FindGameObjectWithTag("Player");
+
+        SpawnTimer -= Time.deltaTime;
+
+        if (SpawnTimer < 0.01)
+        {
+            SpawnDudes();
+        }
+        void SpawnDudes()
+        {
+            basecoord = Random.Range(-37, 134);
+       Instantiate(dudes[(Random.Range(0, dudes.Length))], new Vector3(basecoord, Random.Range(9, 10), 0), Quaternion.identity);
+            SpawnTimer = Random.Range(10.0f, 15.0f);
+        }
+    }
+}
