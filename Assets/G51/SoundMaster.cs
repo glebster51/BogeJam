c6ee5ad519e03aa28be9ddc412f329850136c36c
glebster51 <glebster51@rambler.ru>
2020-01-19 23:33:09 +0800

    no message

---
+++ Assets/G51/SoundMaster.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundMaster : MonoBehaviour
+{
+    static SoundMaster instance = null;
+
+    public static SoundMaster GetMaster()
+    {
+        if (instance == null)
+        {
+            instance = FindObjectOfType<SoundMaster>();
+            if (instance == null)
+                instance = new GameObject("SoundMaster").AddComponent<SoundMaster>();
+        }
+        return instance;
+    }
+
+
+
+    List<Speaker> activeSpeakers = new List<Speaker>();
+    int maximumSpeakersOnline = 3;
+
+
+
+    public bool SpeakerWanaSay(Speaker speaker)
+    {
+        bool canSay = (activeSpeakers.Count < maximumSpeakersOnline);
+        if (canSay)
+        {
+            activeSpeakers.Add(speaker);
+            StartCoroutine(SpeakerSayTimer(speaker, 2f));
+        }
+
+        return canSay;
+    }
+
+    IEnumerator SpeakerSayTimer(Speaker speaker, float timer)
+    {
+        yield return new WaitForSeconds(timer);
+        activeSpeakers.Remove(speaker);
+    }
+}
---
+++ Assets/G51/Speaker.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Speaker : MonoBehaviour
+{
+    public List<AudioClip> sayOnAwake;
+    public List<AudioClip> sayOnHunted;
+    public List<AudioClip> sayOnAttack;
+    public List<AudioClip> sayOnGetDamage;
+    public List<AudioClip> sayOnDead;
+    public SoundMaster master;
+    public AudioSource audio;
+
+    private bool init = false;
+    private void Start()
+    {
+        Init();
+    }
+
+    void Init()
+    {
+        if (init)
+            return;
+        
+        init = true;
+        master = SoundMaster.GetMaster();
+        audio = gameObject.AddComponent<AudioSource>();
+    }
+
+    private void Awake()
+    {
+        Init();
+        TrySay(sayOnAwake);
+    }    
+    
+    public void SayHanted()
+    {
+        TrySay(sayOnHunted);
+    }    
+
+    public void SayAttack()
+    {
+        TrySay(sayOnAttack);
+    }
+    
+    public void SayGetDamage()
+    {
+        TrySay(sayOnGetDamage);
+    }
+
+    public void SayDead()
+    {
+        TrySay(sayOnDead);
+    }
+    
+    //==============================================================
+    void TrySay(List<AudioClip> list)
+    {
+        if (master.SpeakerWanaSay(this))
+            if (list.Count > 0)
+            {
+                audio.clip = list[Random.Range(0, list.Count)];
+                audio.Play();
+            }
+    }
+}
